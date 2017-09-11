c7116af0c7aab753229255f6f3b239b761633e72
GabrielFabian <gabe.fabian@gmail.com>
2017-09-11 10:37:18 -0700

    Completely removed spring from backend. api now spins up mongoDB, populates with dummy student and dummy admin data. 3 endpoints available to get greeting, students, and admins from db.

--- Backend/api/run.py
+++ Backend/api/run.py
@@ -1,6 +1,8 @@
 # run.py
 
 # Programming For All
+# TODO: Divide the routes in to separate files using Blueprints to simplify and modularize the project.
+# http://flask.pocoo.org/docs/0.12/blueprints/
 # Author: Gabriel Fabian, 2017
 
 from models.Person import *
@@ -8,30 +10,57 @@ from models.Admin import *
 from models.Student import *
 from pymongo import MongoClient
 import json
+from bson.json_util import dumps
+from flask import *
 
-def get_db():
-    client = MongoClient('localhost:27017')
-    db = client.ProgrammingForAll
-    return db
 
-def add_student(db, student):
-    db.countries.insert({"name" : "Canada"})
+client = MongoClient('localhost', 27017)
+db = client.ProgrammingForAll
+admins = db.admins
+students = db.students
 
-def add_admin(db, admin):
-    db.countries.insert({"name" : "Canada"})
-    
-def get_country(db):
-    return db.countries.find_one()
+
+app = Flask(__name__)
+title = "Programming for All"
+heading = "PFA"
+
+
+def clean_db():
+    admins.drop()
+    students.drop()
+
+def add_student(student):
+    students.insert(student.__dict__)
+
+def add_admin(admin):
+    admins.insert(admin.__dict__)
+
+@app.route("/")
+def hello():
+    greeting = [{'greeting': 'Hello World!'}]
+    return jsonify({'messages': greeting})
+
+@app.route("/students", methods=['GET'])
+def get_all_students():
+    output = []
+    for student in students.find():
+        output.append({'firstName' : student['firstName'], 'lastName' : student['lastName'], \
+            'id' : student['id'], 'age' : student['age']})
+    return jsonify({'students' : output})
+
+@app.route("/admins", methods=['GET'])
+def get_all_admins():
+    output = []
+    for admin in admins.find():
+        output.append({'firstName' : admin['firstName'], 'lastName' : admin['lastName'], \
+            'id' : admin['id'], 'role' : admin['role']})
+    return jsonify({'students' : output})
+
 
 if __name__ == "__main__":
-
-	dummyStudent = Student("Charlie", "Foo", 1, 12)
-	dummyAdmin = Admin("Melissa", "Bar", 2, "Admin")
-
-	print json.dumps(dummyAdmin.__dict__)
-
-	print dummyStudent
-
-    #db = get_db() 
-    #add_country(db)
-    #print get_country(db)
+    clean_db()
+    dummyAdmin = Admin('Cindy', 'Smith', 2, 'Admin')
+    dummyStudent = Student('Timmy', 'Junior', 1, 12)
+    add_student(dummyStudent)
+    add_admin(dummyAdmin)
+    app.run()
