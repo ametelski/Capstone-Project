62acd0f263326b6b5a9608cf63209eed3951346b
Miguel-Deniz <miguel391@yahoo.com>
2017-10-03 12:39:06 -0700

    Added the getSkill method

--- Backend/api/run.py
+++ Backend/api/run.py
@@ -4,6 +4,8 @@
 # TODO: Divide the routes in to separate files using Blueprints to simplify and modularize the project.
 # http://flask.pocoo.org/docs/0.12/blueprints/
 # Author: Gabriel Fabian, 2017
+# Author: Miguel Deniz
+# Last update: 10/2/17
 
 from models.Person import *
 from models.Admin import *
@@ -36,11 +38,45 @@ def add_student(student):
 def add_admin(admin):
     admins.insert(admin.__dict__)
 
+'''
+    Description:
+        Returns a 'Hello World!' string.
+    
+    Params:
+        None
+        
+    Returns:
+        JSON with String
+'''
 @app.route("/")
 def hello():
     greeting = [{'greeting': 'Hello World!'}]
     return jsonify({'messages': greeting})
 
+
+'''
+    Description:
+        Returns a list of students currently stored in the database
+
+    Params:
+        None
+
+    Returns:
+        JSON
+
+        Example:
+            {
+                "students": [
+                    {
+                        "age": 12, 
+                        "email": "student1@pfa.com", 
+                        "firstName": "Timmy", 
+                        "id": 1, 
+                        "lastName": "Junior"
+                    }
+                ]
+            }
+'''
 @app.route("/students", methods=['GET'])
 def get_all_students():
     output = []
@@ -49,6 +85,30 @@ def get_all_students():
             'id' : student['id'], 'age' : student['age'], 'email' : student['email']})
     return jsonify({'students' : output})
 
+
+'''
+    Description:
+        Returns a list of admins currently stored in the database
+
+    Params:
+        None
+
+    Returns:
+        JSON
+
+        Example:
+            {
+                "admins": [
+                    {
+                        "email": "admin1@pfa.com", 
+                        "firstName": "Cindy", 
+                        "id": 2, 
+                        "lastName": "Smith", 
+                        "role": "Admin"
+                    }
+                ]
+            }
+'''
 @app.route("/admins", methods=['GET'])
 def get_all_admins():
     output = []
@@ -57,6 +117,83 @@ def get_all_admins():
             'id' : admin['id'], 'role' : admin['role'], 'email' : admin['email']})
     return jsonify({'admins' : output})
 
+'''
+    Description:
+        Returns a list of all skills and their statuses for a specific user.
+    
+    Params:
+        userID
+    
+    Returns:
+        JSON
+        
+        {
+            "skills": [
+                {
+                    "skillConcepts": "concepts", 
+                    "skillConceptsCompleted": "45", 
+                    "skillName": "HTML", 
+                    "skillURL": "www.url0.com"
+                }, 
+                {
+                    "skillConcepts": "concepts", 
+                    "skillConceptsCompleted": "23", 
+                    "skillNam": "Python", 
+                    "skillURL": "www.url1.com"
+                }, 
+                {
+                    "skillConcepts": "concepts", 
+                    "skillConceptsCompleted": "78", 
+                    "skillNam": "CSS", 
+                    "skillURL": "www.url2.com"
+                }
+            ]
+        }
+'''
+@app.route("/getSkill/<userID>", methods=['GET'])
+def get_skill(userID = None):
+
+    'TODO: These are just constant values for now, later on they should be retreived from the database using the userID'
+
+    CONST_SKILL_NAME_DESC = "skillName";
+    skillName1 = "HTML";
+    skillName2 = "Python";
+    skillName3 = "CSS";
+
+    CONST_SKILL_URL_DESC = "skillURL";
+    skillURL1 = "www.url1.com";
+    skillURL2 = "www.url2.com";
+    skillURL3 = "www.url3.com";
+
+    CONST_SKILL_COMPLETED_DESC = "skillConceptsCompleted";
+    skillPercentage1 = "43";
+    skillPercentage2 = "100";
+    skillPercentage3 = "0";
+
+    CONST_SKILL_CONCEPTS_DESC = "skillConcepts";
+    concepts1 = "concepts1value";
+    concepts2 = "concepts2value";
+    concepts3 = "concepts3value";
+
+    list = [
+        {CONST_SKILL_NAME_DESC: skillName1, CONST_SKILL_URL_DESC: skillURL1, CONST_SKILL_COMPLETED_DESC: skillPercentage1, CONST_SKILL_CONCEPTS_DESC: concepts1},
+        {CONST_SKILL_NAME_DESC: skillName2, CONST_SKILL_URL_DESC: skillURL2, CONST_SKILL_COMPLETED_DESC: skillPercentage2, CONST_SKILL_CONCEPTS_DESC: concepts2},
+        {CONST_SKILL_NAME_DESC: skillName3, CONST_SKILL_URL_DESC: skillURL3, CONST_SKILL_COMPLETED_DESC: skillPercentage3, CONST_SKILL_CONCEPTS_DESC: concepts3}
+    ]
+    return jsonify(skills = list)
+
+
+'''
+    Description:
+
+    Params:
+
+    Returns:
+
+'''
+@app.route('/getSkillConcept/<userID>', methods=['GET'])
+def get_skill_concept(userID = None, skillType = None):
+    return jsonify({''})
 
 
 def populate_db():
