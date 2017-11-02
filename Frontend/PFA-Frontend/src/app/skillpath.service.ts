ae5e15ab812047ee60d9daaf16f5ed4d55c11db5
Adam Metelski <asmetelski@gmail.com>
2017-11-02 00:06:32 -0700

    changing the endpoints

--- Frontend/PFA-Frontend/src/app/skillpath.service.ts
+++ Frontend/PFA-Frontend/src/app/skillpath.service.ts
@@ -9,6 +9,6 @@ export class SkillpathService {
   constructor(private http: Http) { }
 
   getSkillPath(): Observable <ISkillPath[]> {
-    return this.http.get('http://localhost:5000/').map(res => res.json());
+    return this.http.get('http://127.0.0.1:5000/getSkill?studentID=1').map(res => res.json());
   }
 }
