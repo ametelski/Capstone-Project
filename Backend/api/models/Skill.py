ea7ce08bb7ac2e0e9a3eceaab24e2ed919d1972c
Nguyen Dao <ndao1900@gmail.com>
2017-11-02 22:07:53 -0700

    Added skillConcept editing functionality

--- Backend/api/models/Skill.py
+++ Backend/api/models/Skill.py
@@ -8,11 +8,9 @@ import json
 
 class Skill():
 
-	defaultSkillConcepts = [SkillConcept("Repetition", 1, "to help kids with...", "R1C2", ["www.link1.com", "www.link2.com"], False), \
-							SkillConcept("Condition", 2, "Student Has COMPLETED this MODULE...", "R2C1", ["www.link1.com", "www.link2.com"], True), \
-							SkillConcept("Procedural", 3, "to help kids with...", "R2C3", ["www.link1.com", "www.link2.com"], False)]
+	defaultSkillConcepts = [1,2,3]
 
-	def __init__(self, skillName, skillUrl, skillConcepts=defaultSkillConcepts, skillConceptsCompleted=0):
+	def __init__(self, skillName, skillUrl, skillConcepts=defaultSkillConcepts, skillConceptsCompleted=[1,2]):
 		self.skillName = skillName
 		self.skillUrl = skillUrl
 		self.skillConcepts = skillConcepts
