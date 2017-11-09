650df5353ee15e0cbef6475398732f4901cfa806
Adam Metelski <asmetelski@gmail.com>
2017-11-09 08:36:18 -0700

    Mock data for skill concept

--- Frontend/PFA-Frontend/src/app/skill-tree/skill-tree.component.ts
+++ Frontend/PFA-Frontend/src/app/skill-tree/skill-tree.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ISkillConcept } from '../Models/skillConcept.model';
 import { SkillpathService } from '../skillpath.service';
+import { ISkillPath } from '../Models/skillPath.model';
+import { ISkillConcept } from '../Models/skillConcept.model';
 
 @Component({
   selector: 'app-skill-tree',
@@ -8,19 +9,52 @@ import { SkillpathService } from '../skillpath.service';
   styleUrls: ['./skill-tree.component.css']
 })
 export class SkillTreeComponent implements OnInit {
-  @Input() skillConcept: ISkillConcept[]
+  @Input() skill: String;
 
+  skillConcepts: ISkillConcept[] = [
+    {
+      skillConceptName: 'Concept 1 name',
+      skillDescription: 'This area will contain concepts 1 the detailed description',
+      extLearnLinks: ['www.google.com', 'www.code.com'],
+      completed: false,
+      location: 2.2
+    },
+    {
+      skillConceptName: 'Concept 2 name',
+      skillDescription: 'This area will contain concept 2 detailed description',
+      extLearnLinks: ['www.link3.com', 'www.link4.com'],
+      completed: false,
+      location: 2.2
+    }
+  ];
 
-  // = {
-  //   skillTitle: 'loops',
-  //   skillDescriptionL: 'This module will go over loops',
-  //   extLearnLinks: ['www.google.com'],
-  //   completed: false
-  // }
-  constructor(private skill: SkillpathService) { }
+  constructor(private service: SkillpathService) {}
 
   ngOnInit() {
-
+    this.getSkillTreeConcepts();
   }
 
+  getSkillTreeConcepts() {
+    this.service.getSkillPath().subscribe(data => {
+      debugger;
+      // this.skillConcept = data[0].skillConcepts
+      console.log(data[0]);
+    });
+  }
+
+  getName(num) {
+    return this.skillConcepts[num].skillConceptName;
+  }
+
+  getDescription(num) {
+    return this.skillConcepts[num].skillDescription;
+  }
+
+  getExternalLinks(num) {
+    return this.skillConcepts[num].extLearnLinks;
+  }
+
+  getCompletion(num) {
+    return this.skillConcepts[num].completed;
+  }
 }
